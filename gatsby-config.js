40fefcf852c48df53aeb7bb6e647f5813187d2da
Blake Collins <blakeac369@gmail.com>
2021-02-09 15:38:39 -0800

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Regular Joe Coffee Shop`,
@@ -13,6 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+      { resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }, 
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -22,7 +32,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#ff9e09`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
